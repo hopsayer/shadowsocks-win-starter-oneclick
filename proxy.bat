2e584a5251d8e4a6237955cc10675d96faf33afe
hopsayer <84712356+hopsayer@users.noreply.github.com>
2025-11-16 10:58:12 +0300

    Update proxy.bat

--- proxy.bat
+++ proxy.bat
@@ -1,24 +1,24 @@
 @echo off
 setlocal
 
-rem Запускаем shadowsocks локально в фоне
+rem Run shadowsocks locally in background
 start "" /b .\sslocal.exe -c .\config.json
 
-rem Ждем 0.1 секунды, чтобы прокси поднялся
+rem Wait to ensure that proxy is up
 ping -n 1 127.0.0.1 >nul
 
-rem Включаем системный прокси на 127.0.0.1:1080
+rem Enable system proxy on 127.0.0.1:1080
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d "127.0.0.1:1080" /f
 
 echo System proxy enabled on 127.0.0.1:1080
-echo In order to disable and exit, close this window.
+echo PRESS ANY KEY TO QUIT PROPERLY
 
-rem Ждем нажатия любьой клавиши (окно на этом этапе закрывать не надо)
+rem Wait for any key pressed
 echo Press any key to disable system proxy 
 pause >nul
 
-rem Выключаем системный прокси (возвращаем в дефолт)
+rem Disable system proxy (setting it to default state)
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /f >nul 2>&1
 
@@ -26,5 +26,6 @@ echo System proxy disabled
 taskkill /im sslocal.exe /f >nul 2>&1
 endlocal
 exit
+
 
 
